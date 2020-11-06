291c21152b4f2018b9145bbe2e8e98cd22270063
tkchoo90 <e0533379@u.nus.edu>
2020-11-06 22:01:08 +0800

    hello again!

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) =>{
     res.send("Hello World!");
 });
 
+app.get("/2", (req, res) =>{
+    res.send("Hello again!!");
+});
+
 app.listen(3000, () =>{
     console.log("Server running at port 3000");
 })
